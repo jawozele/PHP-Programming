7013f0698aa9fea8b700c5a75dd3ec5fa4b5685d
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-04-17 19:50:19 +0100

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,26 @@
+<!-- This program will print my full name saved in a variable.
+It will also print my age saved in a variable and a string 
+which will display the name of the university I study at -->
+
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Welcome</title>
+	</head>
+	<body>
+
+<?php
+
+$name = "Josh Awozele";
+
+echo $name; echo "<br>";
+$age = 100;
+echo $age; echo "<br>";
+echo "I study at the University of Northampton";
+
+
+
+?>
+	</body>
+
+</html>
